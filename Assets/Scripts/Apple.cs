b9a0148aeb710f5f8b63c689fec21dfcd8b661d7
Ekaterina <burlutskayaekaterinka@yandex.ru>
2025-04-05 14:30:03 +0500

    Scripts added

---
+++ Assets/Scripts/Apple.cs
@@ -0,0 +1,15 @@
+﻿using Unity.VisualScripting;
+
+namespace Calculator.UI
+{
+    public class Apple
+    {
+        private string Sign;
+
+        private void Start()
+        {
+            
+        }
+        
+    }
+}
